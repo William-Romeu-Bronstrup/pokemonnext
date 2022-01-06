3310c7b5eda7d1b27b461290f07eab588a6b8341
William <williambronstrup13@gmail.com>
2022-01-05 22:01:39 -0300

    Configurando

---
+++ pages/components/Layout.js
@@ -0,0 +1,18 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+import Head from "next/head";
+
+export default function Layout({ children }) {
+  return (
+    <>
+    <Head>
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+      <title>PokemonNext</title>
+    </Head>
+      <NavBar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
