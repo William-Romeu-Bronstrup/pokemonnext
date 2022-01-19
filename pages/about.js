81de4e26b696b4660b6182b208bf6122273dfb3b
William <williambronstrup13@gmail.com>
2022-01-18 23:55:07 -0300

    Fazendo o Fecth pra API

--- pages/about.js
+++ pages/about.js
@@ -6,8 +6,14 @@ export default function About() {
   return (
     <div className={styles.Container}>
       <h1>Sobre o Projeto</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque odit commodi veritatis esse! Nostrum omnis vero architecto a repellat? Debitis perspiciatis minima cumque modi harum corrupti laudantium repellendus fuga mollitia.</p>
-      <Image src="/images/charizard.png" width="300" height="300" alt="Charizard" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque odit commodi veritatis esse! Nostrum omnis vero architecto a repellat? Debitis perspiciatis minima cumque modi harum corrupti laudantium repellendus fuga mollitia.
+      </p>
+      <Image
+        src="/images/charizard.png"
+        width="300"
+        height="300"
+        alt="Charizard"
+      />
     </div>
   )
 }
